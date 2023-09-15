04b3dddf7ce0321c6b9989507431ce03b506541f
abbynaoi <106639645+abbynaoi@users.noreply.github.com>
2023-09-15 18:39:48 +0900

    tiptap: テーブル再現

--- src/components/Tiptap/TiptapEditor/TiptapEditor.tsx
+++ src/components/Tiptap/TiptapEditor/TiptapEditor.tsx
@@ -1,11 +1,19 @@
 import { allHavingElementsHtml } from '../../../mockdata/html'
 import { EditorProvider, FloatingMenu, BubbleMenu } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import Table from '@tiptap/extension-table'
+import TableRow from '@tiptap/extension-table-row'
+import TableHeader from '@tiptap/extension-table-header'
+import TableCell from '@tiptap/extension-table-cell'
 import { TiptapMenuBar } from '../TiptapMenubar/TiptapMenubar'
 
 // define your extension array
 const extensions = [
   StarterKit,
+  Table.configure({ resizable: true }),
+  TableHeader,
+  TableRow,
+  TableCell,
 ]
 
 const Tiptap = () => {
